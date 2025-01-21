308d80f62ebd7a342f39fcc52f4394c184c827ca
Francisco José Solana <148755649+Fsolanaa@users.noreply.github.com>
2025-01-21 09:53:55 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  posts: any[] = [];
+  constructor(private dataService: DataService) {
+
+  }
+  ngOnInit(): void {
+    this.dataService.getPost().subscribe(
+      data => this.posts = data,
+      error => console.error('Error fetching data:', error)
+    );
+  }
+
+  }
